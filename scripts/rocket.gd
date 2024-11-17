2d1cc5b1913b346e936baba63db6e9a18394cf72
t3mp0r4ry23 <80374341+t3mp0r4ry23@users.noreply.github.com>
2024-11-16 21:16:29 -0800

    complete launch sequence

--- scripts/rocket.gd
+++ scripts/rocket.gd
@@ -5,6 +5,7 @@ const turn = float(0)
 var launchstart := false
 var expo = 0
 var com = 40
+var force_point = Vector2(0, -180)
 
 func _on_button_pressed() -> void:
 	launchstart = true
@@ -14,10 +15,17 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	if launchstart:
 		if v_extra.y == 0:
 			v_extra.y = -0.25
-		elif v_extra.y > (-1000*(2**expo)):
+		elif v_extra.y > (-2000*(2**expo)):
 			v_extra.y += v_extra.y * (1.25 * (2**expo))
-		print(v_extra)
-	state.apply_force(v_extra, Vector2(0, -180))
+		if position.y <= -2500 && force_point.x > -180:
+			force_point.x -= 1
+			force_point.y += 1
+		if force_point.x == -180:
+			v_extra = Vector2(0,0)
+		if rotation_degrees >= 90:
+			rotation_degrees = 90
+		print(position)
+	state.apply_force(v_extra, force_point)
 
 
 func _on_option_button_item_selected(index: int) -> void:
